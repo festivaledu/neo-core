e918330e968328b87852e684d87afe3f2028985d
Väinämö Łūmikērø <vainamo@git.festival.ml>
2018-12-20 13:20:01 +0100

    Add class for AES key and vector

---
+++ Cryptography/CryptographicData.cs
@@ -0,0 +1,19 @@
+﻿namespace Neo.Core.Cryptography
+{
+    public struct CryptographicData
+    {
+        public byte[] AesKey { get; }
+
+        // ReSharper disable once InconsistentNaming
+        public byte[] AesIV { get; }
+        
+
+        // ReSharper disable once InconsistentNaming
+        public CryptographicData(byte[] aesKey, byte[] aesIV) {
+            AesKey = aesKey;
+            AesIV = aesIV;
+        }
+
+        public bool IsValid() => AesKey != null && AesKey.Length != 0 && AesIV != null && AesIV.Length != 0;
+    }
+}
