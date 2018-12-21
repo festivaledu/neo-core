fb05371d2ac7fccf002f8c9447f1a919359b7f86
Väinämö Łūmikērø <vainamo@git.festival.ml>
2018-12-21 12:15:18 +0100

    Add field to get groups from member

--- Shared/Member.cs
+++ Shared/Member.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Neo.Core.Shared
 {
@@ -6,6 +8,8 @@ namespace Neo.Core.Shared
     {
         // TODO
         public Account Account { get; set; }
+
+        public List<Group> Groups => Pool.Server.Groups.FindAll(g => g.IsMember(this)).OrderBy(g => g.SortValue).ToList();
 
         public new Guid InternalId => Account.InternalId;
     }
