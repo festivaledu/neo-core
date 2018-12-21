b88692e985081f19a5727c3d46e1976f71e68b19
Väinämö Łūmikērø <vainamo@git.festival.ml>
2018-12-21 12:01:27 +0100

    Add LocaleManager

--- Localization/LocaleManager.cs
+++ Localization/LocaleManager.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using Neo.Core.Shared;
 using Newtonsoft.Json;
-using WebSocketSharp;
 
 namespace Neo.Core.Localization
 {
@@ -22,121 +22,94 @@ namespace Neo.Core.Localization
         private static readonly Lazy<LocaleManager> instance = new Lazy<LocaleManager>(() => new LocaleManager());
 
         /// <summary>
-        ///     Gets an existing value from the config or sets a new one.
-        ///     <para>
-        ///         Setting a new value will override the existing one. If <see cref="Load"/> has been called at least once, the config will be saved automatically.
-        ///     </para>
+        ///     Gets an existing value from the locale file.
         /// </summary>
+        /// <param name="locale">The locale to load the value from.</param>
         /// <param name="key">The key connected to the value.</param>
         /// <returns>Returns the value connected to the <see cref="key"/>.</returns>
-        /// <exception cref="KeyNotFoundException">Thrown if the loaded config does not contain <see cref="key"/>.</exception>
-        public dynamic this[string key] {
-            get => configValues[key];
-            set {
-                configValues[key] = value;
-                Save(filePath);
-            }
-        }
+        /// <exception cref="KeyNotFoundException">Thrown if the loaded locale file does not contain <see cref="locale"/> or <see cref="key"/>.</exception>
+        public string this[string locale, string key] => localizedValues[locale][key];
 
         /// <summary>
-        ///     Gets an existing value from the config or a default value if <see cref="key"/> doesn't exist.
-        ///     <para>
-        ///         The <see cref="defaultValue"/> will be set to the <see cref="key"/> if no value existed yet. If <see cref="Load"/> has been called at least once, the config will be saved automatically.
-        ///     </para>
+        ///     Gets an existing value from the locale file or a default value if <see cref="key"/> doesn't exist.
         /// </summary>
+        /// <param name="locale">The locale to load the value from.</param>
         /// <param name="key">The key connected to the value.</param>
-        /// <param name="defaultValue">The value to return and set as the default value if no value existed yet.</param>
-        /// <returns>Returns the value connected to the <see cref="key"/> or <see cref="defaultValue"/> if no value existed yet.</returns>
-        public dynamic this[string key, dynamic defaultValue] {
+        /// <param name="defaultValue">The value to return if no value exists.</param>
+        /// <returns>Returns the value connected to the <see cref="key"/> or <see cref="defaultValue"/> if no value exists.</returns>
+        public string this[string locale, string key, string defaultValue] {
             get {
-                if (configValues.ContainsKey(key)) {
-                    return configValues[key];
+                if (localizedValues.ContainsKey(locale)) {
+                    if (localizedValues[locale].ContainsKey(key)) {
+                        return localizedValues[locale][key];
+                    }
                 }
-
-                configValues[key] = defaultValue;
-                Save(filePath);
 
                 return defaultValue;
             }
         }
 
-        private Dictionary<string, object> configValues = new Dictionary<string, object>();
-        private string filePath;
+        private Dictionary<string, SortedDictionary<string, string>> localizedValues = new Dictionary<string, SortedDictionary<string, string>>();
 
         private LocaleManager() { }
 
         /// <summary>
-        ///     Gets an existing value from the config.
+        ///     Gets an existing value from the locale file.
         /// </summary>
-        /// <typeparam name="TOut">The type to convert the value to.</typeparam>
+        /// <param name="locale">The locale to load the value from.</param>
         /// <param name="key">The key connected to the value.</param>
-        /// <returns>Returns the value connected to the <see cref="key"/> as <see cref="TOut"/>.</returns>
-        /// <exception cref="KeyNotFoundException">Thrown if the loaded config does not contain <see cref="key"/>.</exception>
-        public TOut GetValue<TOut>(string key) {
-            return (TOut) configValues[key];
+        /// <returns>Returns the value connected to the <see cref="key"/>.</returns>
+        /// <exception cref="KeyNotFoundException">Thrown if the loaded locale file does not contain <see cref="locale"/> or <see cref="key"/>.</exception>
+        public string GetValue(string locale, string key) {
+            return localizedValues[locale][key];
         }
 
         /// <summary>
-        ///     Gets an existing value from the config or a default value if <see cref="key"/> doesn't exist.
-        ///     <para>
-        ///         The <see cref="defaultValue"/> will be set to the <see cref="key"/> if no value existed yet. If <see cref="Load"/> has been called at least once, the config will be saved automatically.
-        ///     </para>
+        ///     Gets an existing value from the locale file or a default value if <see cref="key"/> doesn't exist.
         /// </summary>
-        /// <typeparam name="TOut">The type to convert the value to.</typeparam>
+        /// <param name="locale">The locale to load the value from.</param>
         /// <param name="key">The key connected to the value.</param>
-        /// <param name="defaultValue">The value to return and set as the default value if no value existed yet.</param>
-        /// <returns>Returns the value connected to the <see cref="key"/> or <see cref="defaultValue"/> if no value existed yet.</returns>
-        public TOut GetValue<TOut>(string key, TOut defaultValue) {
-            if (configValues.ContainsKey(key)) {
-                return (TOut) configValues[key];
+        /// <param name="defaultValue">The value to return if no value exists.</param>
+        /// <returns>Returns the value connected to the <see cref="key"/> or <see cref="defaultValue"/> if no value exists.</returns>
+        public string GetValue(string locale, string key, string defaultValue) {
+            if (localizedValues.ContainsKey(locale)) {
+                if (localizedValues[locale].ContainsKey(key)) {
+                    return localizedValues[locale][key];
+                }
             }
-
-            configValues[key] = defaultValue;
-            Save(filePath);
 
             return defaultValue;
         }
 
         /// <summary>
-        ///     Loads an existing config from a file.
+        ///     Loads all existing localization dictionary from a folder.
         /// </summary>
-        /// <param name="path">The path to the file.</param>
+        /// <param name="path">The path to the folder.</param>
         /// <exception cref="FormatException">Thrown if the file does not contain a valid configuration.</exception>
         public void Load(string path) {
-            if (File.Exists(path)) {
-                try {
-                    configValues = JsonConvert.DeserializeObject<Dictionary<string, object>>(File.ReadAllText(path));
-                    filePath = path;
-                } catch {
-                    throw new FormatException("This file is not a valid configuration file.");
+            if (Directory.Exists(path)) {
+                foreach (var file in Directory.EnumerateFiles(path)) {
+                    try {
+                        var localeValues = JsonConvert.DeserializeObject<SortedDictionary<string, string>>(File.ReadAllText(file));
+                        var fileInfo = new FileInfo(file);
+
+                        localizedValues.Add(fileInfo.Name.Split('.')[0], localeValues);
+                    } catch {
+
+                    }
                 }
-            } else {
-                filePath = path;
-                Save(path);
             }
         }
+    }
 
-        /// <summary>
-        ///     Saves the current config to a file.
-        /// </summary>
-        /// <param name="path">The path to the file.</param>
-        public void Save(string path) {
-            if (!path.IsNullOrEmpty()) {
-                File.WriteAllText(path, JsonConvert.SerializeObject(configValues, Formatting.Indented));
-            }
+    public static class LocaleExtensions
+    {
+        public static string GetValue(this User user, string key) {
+            return LocaleManager.Instance.GetValue(user.Locale, key);
         }
 
-        /// <summary>
-        ///     Sets a new value to the config.
-        ///     <para>
-        ///         Setting a new value will override the existing one. If <see cref="Load"/> has been called at least once, the config will be saved automatically.
-        ///     </para>
-        /// </summary>
-        /// <param name="key">The key connected to the value.</param>
-        /// <param name="value">The value to set.</param>
-        public void SetValue(string key, dynamic value) {
-            configValues[key] = value;
-            Save(filePath);
+        public static string GetValue(this User user, string key, string defaultValue) {
+            return LocaleManager.Instance.GetValue(user.Locale, key, defaultValue);
         }
     }
 }
