f0d6b1d42fe0525518ff292331242b49853163f1
Väinämö Łūmikērø <vainamo@git.festival.ml>
2018-12-28 18:26:18 +0100

    Add attribute for easy event registration

--- Extensibility/Events/EventListenerAttribute.cs
+++ Extensibility/Events/EventListenerAttribute.cs
@@ -5,5 +5,10 @@ namespace Neo.Core.Extensibility.Events
     [AttributeUsage(AttributeTargets.Method)]
     public class EventListenerAttribute : Attribute
     {
+        public EventType Type { get; set; }
+
+        public EventListenerAttribute(EventType type) {
+            this.Type = type;
+        }
     }
 }
