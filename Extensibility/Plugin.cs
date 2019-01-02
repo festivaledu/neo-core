02d58f30e372bd3d4d680dd2a4c506784c5334dd
Väinämö Łūmikērø <vainamo@git.festival.ml>
2019-01-02 11:40:35 +0100

    Add basic Plugin class and loader for testing

---
+++ Extensibility/Plugin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using Neo.Core.Extensibility.Events;
+using Neo.Core.Shared;
+
+namespace Neo.Core.Extensibility
+{
+    public abstract class Plugin
+    {
+        public abstract string Namespace { get; }
+    }
+}
---
+++ Extensibility/PluginLoader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Neo.Core.Extensibility
+{
+    public static class PluginLoader
+    {
+        public static List<Plugin> Plugins { get; } = new List<Plugin>();
+
+        public static void InitializePlugin(string path) {
+            if (File.Exists(path) && new FileInfo(path).Extension == ".dll") {
+                Assembly.LoadFile(path);
+                var assemblyTypes = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetTypes()).Where(t => typeof(Plugin).IsAssignableFrom(t) && t.IsClass).ToArray();
+
+                foreach (var type in assemblyTypes) {
+                    if (type.Attributes.HasFlag(TypeAttributes.Abstract)) {
+                        continue;
+                    }
+
+                    var plugin = (Plugin) Activator.CreateInstance(type);
+                    
+                    if (Plugins.Any(p => p.Namespace == plugin.Namespace)) {
+                        plugin = null;
+                        continue;
+                    }
+
+                    EventService.RegisterListeners(type, plugin);
+
+                    Plugins.Add(plugin);
+                }
+            }
+        }
+    }
+}
