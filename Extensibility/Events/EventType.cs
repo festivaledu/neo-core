9a8025586aa12bd80e00eeecf4557b3b9f961540
Väinämö Łūmikērø <vainamo@git.festival.ml>
2019-01-02 11:55:32 +0100

    Define more events

--- Extensibility/Events/EventType.cs
+++ Extensibility/Events/EventType.cs
@@ -3,5 +3,35 @@
     public enum EventType
     {
         Custom,
+
+        BeforeAccountCreate,
+        AccountCreated,
+        BeforeChannelCreate,
+        ChannelCreated,
+        BeforeGroupCreate,
+        GroupCreated,
+
+        BeforeAccountEdit,
+        AccountEdited,
+        BeforeChannelEdit,
+        ChannelEdited,
+        BeforeGroupEdit,
+        GroupEdited,
+        BeforeServerEdit,
+        ServerEdited,
+
+        BeforeChannelJoin,
+        ChannelJoined,
+        BeforeGroupJoin,
+        GroupJoined,
+        BeforeServerJoin,
+        ServerJoined,
+
+        BeforeChannelLeave,
+        ChannelLeft,
+        BeforeGroupLeave,
+        GroupLeft,
+        BeforeServerLeave,
+        ServerLeft
     }
 }
---
+++ Extensibility/Events/JoinElementEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using Neo.Core.Shared;
+
+namespace Neo.Core.Extensibility.Events
+{
+    public class JoinElementEventArgs<TElement> : ICancellableEvent
+    {
+        public User Joiner { get; }
+        public TElement Element { get; }
+
+        public JoinElementEventArgs(User joiner, TElement element) {
+            this.Joiner = joiner;
+            this.Element = element;
+        }
+    }
+}
