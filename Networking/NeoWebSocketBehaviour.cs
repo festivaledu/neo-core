195b2a733ff4a90ed45c634da6361cd44c77807d
Väinämö Łūmikērø <vainamo@git.festival.ml>
2019-01-08 10:19:13 +0100

    Fix more async stuff

--- Networking/NeoWebSocketBehaviour.cs
+++ Networking/NeoWebSocketBehaviour.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading.Tasks;
 using Neo.Core.Communication;
 using Neo.Core.Cryptography;
 using Neo.Core.Shared;
@@ -15,24 +14,18 @@ namespace Neo.Core.Networking
     internal class NeoWebSocketBehaviour : WebSocketBehavior
     {
         protected override void OnOpen() {
-            Task.Run(async () => {
-                await Pool.Server.OnConnect(new Client(ID, Context.WebSocket));
-                Pool.Server.SessionManager = Sessions;
-            });
+            Pool.Server.OnConnect(new Client(ID, Context.WebSocket));
+            Pool.Server.SessionManager = Sessions;
         }
 
         protected override void OnClose(CloseEventArgs e) {
-            Task.Run(async () => {
-                await Pool.Server.OnDisconnect(ID, e.Code, e.Reason, e.WasClean);
-                Pool.Server.SessionManager = Sessions;
-            });
+            Pool.Server.OnDisconnect(ID, e.Code, e.Reason, e.WasClean);
+            Pool.Server.SessionManager = Sessions;
         }
 
         protected override void OnError(ErrorEventArgs e) {
-            Task.Run(async () => {
-                await Pool.Server.OnError(ID, e.Exception, e.Message);
-                Pool.Server.SessionManager = Sessions;
-            });
+            Pool.Server.OnError(ID, e.Exception, e.Message);
+            Pool.Server.SessionManager = Sessions;
         }
 
         protected override void OnMessage(MessageEventArgs e) {
