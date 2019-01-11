6d937a1d6e763b53a9a3d7bef54151aa801b68e3
Väinämö Łūmikērø <vainamo@git.festival.ml>
2019-01-11 14:23:45 +0100

    Add more fields to config

--- Config/ConfigValues.cs
+++ Config/ConfigValues.cs
@@ -7,10 +7,13 @@ namespace Neo.Core.Config
 {
     public class ConfigValues
     {
+        public bool GuestsAllowed { get; set; } = false;
+        public bool RegistrationAllowed { get; set; } = false;
         public Identity RootIdentity { get; set; } = new Identity { Id = "neo", Name = "Neo" };
         public byte[] RootPassword { get; set; } = NeoCryptoProvider.Instance.Sha512ComputeHash("LT6SUK7JBELX3UAL");
         public int RSAKeySize { get; set; } = 4096;
         public string ServerAddress { get; set; } = "0.0.0.0";
+        public string ServerName { get; set; } = "Neo Server";
         public int ServerPort { get; set; } = 42420;
     }
 }
