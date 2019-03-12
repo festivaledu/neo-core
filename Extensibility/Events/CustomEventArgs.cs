1708d760fe082373fe0957b4ad6606f1fe2d73d4
Väinämö Łūmikērø <vainamo@git.festival.ml>
2019-03-12 14:17:16 +0100

    Adjust CustomEvent

--- Extensibility/Events/CustomEventArgs.cs
+++ Extensibility/Events/CustomEventArgs.cs
@@ -1,4 +1,6 @@
-﻿namespace Neo.Core.Extensibility.Events
+﻿using System;
+
+namespace Neo.Core.Extensibility.Events
 {
     /// <summary>
     ///     Provides the data for the <see cref="EventType.Custom"/> event.
@@ -11,6 +13,11 @@
         public string Name { get; }
 
         /// <summary>
+        ///     The internal id of the sender of this <see cref="CustomEventArgs"/>.
+        /// </summary>
+        public Guid Sender { get; set; }
+
+        /// <summary>
         ///     The content of this <see cref="CustomEventArgs"/>.
         /// </summary>
         public object[] Content { get; }
@@ -20,9 +27,11 @@
         ///     Initializes a new instance of the <see cref="CustomEventArgs"/> class.
         /// </summary>
         /// <param name="name">The name of this <see cref="CustomEventArgs"/>.</param>
+        /// <param name="sender">The internal id of the sender of this <see cref="CustomEventArgs"/>.</param>
         /// <param name="content">The content of this <see cref="CustomEventArgs"/>.</param>
-        public CustomEventArgs(string name, params object[] content) {
+        public CustomEventArgs(string name, Guid sender, params object[] content) {
             this.Name = name;
+            this.Sender = sender;
             this.Content = content;
         }
     }
